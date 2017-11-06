90c592b914d715e906dcf08c487dfc882e70181a
rschlueter <rschlueter98@gmail.com>
2017-11-06 14:52:33 -0500

    submission for long term testing

--- findFPSES.py
+++ findFPSES.py
@@ -12,14 +12,14 @@ def downloadImages(inputFile):
         try:
             stream = cv2.VideoCapture(line)
             ti = time.time()
-            while((time.time()-ti)<10):
+            while((time.time()-ti)<120):
                 frame = stream.read()[1]
-                if((time.time()-ti>5)):
+                if((time.time()-ti>75)):
                     count=count+1
 
             temp = line.split("/")[4]
             temp = temp.split(".")[0]
-            FPS = count/5
+            FPS = count/45
 
             print(str(temp) + "\t" + str(FPS))
             outputFile.write(str(temp) + "\t" + str(FPS))
