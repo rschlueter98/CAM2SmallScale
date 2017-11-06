1e965ec2fac23d35d7f7749c706538a38e9cda07
rschlueter <rschlueter98@gmail.com>
2017-11-06 14:28:22 -0500

    initial addition

---
+++ findFPSES.py
@@ -0,0 +1,32 @@
+import cv2
+import time
+
+
+
+
+
+
+def downloadImages(inputFile):
+    for line in inputFile:
+        count=0
+        cap = cv2.VideoCapture(url)
+        ti = time.time()
+        while((time.time()-ti)<60):
+            frame = stream.read()[1]
+            if((time.time()-ti>30)):
+                count=count+1
+
+        temp = line.split("/")[4]
+        temp = temp.split(".")[0]
+        FPS = count/30
+
+        outputFile.write(temp)
+        outputFile.write(FPS)
+
+if __name__ == '__main__':
+  inputFile = open("m3u8s.txt", 'r')
+  outputFile = open("wholeSetFPSES.txt", 'w')
+  downloadImages(inputFile)
+
+  # Load yolo model and start analysis
+  # loadAnalysis()
