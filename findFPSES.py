cc16acbc2d702446e42ce2fb9832b956a1471671
rschlueter <rschlueter98@gmail.com>
2017-11-06 14:48:06 -0500

    added failed print statements for bad links

--- findFPSES.py
+++ findFPSES.py
@@ -9,21 +9,24 @@ import time
 def downloadImages(inputFile):
     for line in inputFile:
         count=0
-        stream = cv2.VideoCapture(line)
-        # print("New Stream")
-        ti = time.time()
-        while((time.time()-ti)<10):
-            frame = stream.read()[1]
-            # print("Frame Downloaded")
-            if((time.time()-ti>5)):
-                count=count+1
+        try:
+            stream = cv2.VideoCapture(line)
+            ti = time.time()
+            while((time.time()-ti)<10):
+                frame = stream.read()[1]
+                if((time.time()-ti>5)):
+                    count=count+1
 
-        temp = line.split("/")[4]
-        temp = temp.split(".")[0]
-        FPS = count/5
+            temp = line.split("/")[4]
+            temp = temp.split(".")[0]
+            FPS = count/5
 
-        print(str(temp) + "\t" + str(FPS))
-        outputFile.write(str(temp) + "\t" + str(FPS))
+            print(str(temp) + "\t" + str(FPS))
+            outputFile.write(str(temp) + "\t" + str(FPS))
+        except:
+            print(str(temp) + "\t" + "failed")
+            outputFile.write(str(temp) + "\t" + "failed")
+
 
 if __name__ == '__main__':
   inputFile = open("m3u8s.txt", 'r')
