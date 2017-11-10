be66c82f283eb5513b4b6a605777d2d82adf01ef
rschlueter <rschlueter98@gmail.com>
2017-11-10 13:14:56 -0500

    fixed time and added a print statement

--- week1Capture2.py
+++ week1Capture2.py
@@ -9,8 +9,8 @@ RESULTS_PATH = "/homes/rschluet/SaraCode"
 
 def checkCamera():
     source = "http://video3.earthcam.com/fecnetwork/4054.flv/playlist.m3u8"
-    duration = 300 #611000 (1 week)
-    interval = 60 # 600 (10 minute break)
+    duration = 611000 #300 #611000 (1 week)
+    interval = 600 #60 # 600 (10 minute break)
     folderName = source.split("/")[4]
     #folderName = source.split("/")[2] #for IP Cameras
     cam_directory = os.path.join(RESULTS_PATH, str(folderName))
@@ -36,6 +36,7 @@ def checkCamera():
             # Save the image.
             file_name = '{}/{}.jpg'.format(cam_directory,datetime.datetime.fromtimestamp(
                         capture_time).strftime('%Y-%m-%d_%H-%M-%S-%f'))
+            print(str(file_name))
             cv2.imwrite(str(file_name), frame)
             c = c + 1
             vc.release()
